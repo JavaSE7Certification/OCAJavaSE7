f59b3d459bbff2feb0a7e0ec3cfb3fd6b535c4d1
Nazar Oprysk <noprysk@Nazars-MacBook-Pro.local>
2014-10-28 22:22:53 +0200

    Chapter 1

--- ocajavase7/src/main/java/nao/cycledev/ocajavase7/MainApp.java
+++ ocajavase7/src/main/java/nao/cycledev/ocajavase7/MainApp.java
@@ -1,7 +1,10 @@
 package nao.cycledev.ocajavase7;
 
-/**
- * Created by noprysk on 10/22/14.
- */
 public class MainApp {
+
+      static public void main(String... argsttt){
+
+        System.out.println("Test");
+
+    }
 }
--- ocajavase7/src/main/java/nao/cycledev/ocajavase7/c1/javabasic/JavaBasic.java
+++ ocajavase7/src/main/java/nao/cycledev/ocajavase7/c1/javabasic/JavaBasic.java
@@ -1,7 +1,7 @@
 package nao.cycledev.ocajavase7.c1.javabasic;
 
-/**
- * Created by noprysk on 10/22/14.
- */
 public class JavaBasic {
+
+
+
 }
